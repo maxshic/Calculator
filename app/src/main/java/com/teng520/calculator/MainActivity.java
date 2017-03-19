3b340d80fa8a62d824031e6827dcbc9042ad34b6
最爱滕小厨 <teng520jiao@qq.com>
2017-03-19 10:17:57 +0800

    hehe
    haha

--- app/src/main/java/com/teng520/calculator/MainActivity.java
+++ app/src/main/java/com/teng520/calculator/MainActivity.java
@@ -3,6 +3,8 @@ package com.teng520.calculator;
 import android.support.v7.app.AppCompatActivity;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +17,7 @@ public class MainActivity extends Activity {
     private Spinner spinner;
     private Button button;
     private TextView textView;
+    private String str;
     private String[] opera=getResources().getStringArray(R.array.operator);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +28,42 @@ public class MainActivity extends Activity {
         spinner=(Spinner)findViewById(R.id.spinner);
         button=(Button)findViewById(R.id.button);
         textView=(TextView)findViewById(R.id.result);
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,opera);
+        ArrayAdapter<String> adapter=new ArrayAdapter(this,android.R.layout.simple_spinner_item,opera);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                str=adapterView.getItemAtPosition(i).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float num1=Float.valueOf(editText1.getText().toString());
+                float num2=Float.valueOf(editText2.getText().toString());
+                switch (str){
+                    case "+":
+                        textView.setText(Float.toString(num1+num2));
+                        break;
+                    case "-":
+                        textView.setText(Float.toString(num1-num2));
+                        break;
+                    case "*":
+                        textView.setText(Float.toString(num1*num2));
+                        break;
+                    case "/":
+                        textView.setText(Float.toString(num1/num2));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
     }
 }
